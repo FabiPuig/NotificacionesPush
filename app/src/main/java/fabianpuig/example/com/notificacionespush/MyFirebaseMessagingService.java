11b91e41da1084cafcd8181c6c2f108c7b462940
Fabian <20464654j@iespoblenou.org>
2018-03-06 20:25:59 +0100

    Mostramos una notificación con layout personalizado cuando tenemos la app en primer plano

--- app/src/main/java/fabianpuig/example/com/notificacionespush/MyFirebaseMessagingService.java
+++ app/src/main/java/fabianpuig/example/com/notificacionespush/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package fabianpuig.example.com.notificacionespush;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.media.Ringtone;
@@ -7,6 +8,7 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -31,8 +33,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             String titulo = remoteMessage.getNotification().getTitle();
             String texto = remoteMessage.getNotification().getBody();
 
-            //Mostramos la notificación en la barra de estado
-            showNotification(titulo, texto);
+
         }
         // Mensaje con carga de datos
         if(remoteMessage.getData() != null) {
@@ -40,35 +41,43 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             //log solo por testeo
             Log.i( Controller.tag, "Usuario: " + remoteMessage.getData().get( "usuario" ) );
 
+            //Mostramos la notificación en la barra de estado
+            showNotification( remoteMessage.getData().get( "usuario" ),
+                    remoteMessage.getData().get( "texto" ));
         }
     }
 
     /** Método para mostrar las notificaciones con la app en primer plano
      *
-     * @param title
+     * @param user
      * @param text
      */
-    private void showNotification(String title, String text) {
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.stat_sys_warning)
-                        .setContentTitle(title)
-                        .setContentText(text);
+    private void showNotification(String user, String text) {
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0, notificationBuilder.build());
+        // custom layout
+        RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_push);
+        contentView.setImageViewResource(R.id.image, R.drawable.user_01 );
+        contentView.setTextViewText(R.id.title, user + " te manda un mensaje:");
+        contentView.setTextViewText(R.id.text, text);
 
-        // sonido
-        try {
-            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-            r.play();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // icono a mostrar en la status bar y contenido (custom layout)
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.user_01)
+                .setContent(contentView);
+
+        // Notificación que mostrará
+        Notification notification = mBuilder.build();
+        // esconde la notificación después de seleccionarla
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        // sonido por defecto de la notificación
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        // vibración por defecto de la notificacion
+        notification.defaults |= Notification.DEFAULT_VIBRATE;
+        notificationManager.notify(1, notification);
+
 
     }
 }
